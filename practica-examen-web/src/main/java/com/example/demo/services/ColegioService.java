744146a131387a00fe823a71fb514540b12b58d0
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-11 14:08:22 -0500

    Modificado ColegioService para recibir dto

--- practica-examen-web/src/main/java/com/example/demo/services/ColegioService.java
+++ practica-examen-web/src/main/java/com/example/demo/services/ColegioService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dtos.ColegioDto;
 import com.example.demo.entities.Colegio;
+import com.example.demo.entities.Municipio;
 import com.example.demo.repositories.ColegioRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -16,6 +18,9 @@ public class ColegioService {
 	@Autowired
 	private ColegioRepository colegioRepository;
 	
+	@Autowired
+	private MunicipioService municipioService;
+	
 	public List<Colegio> getAllColegios() {
 		return colegioRepository.findAll();
 	}
@@ -24,13 +29,30 @@ public class ColegioService {
 		return colegioRepository.getReferenceById(id);
 	}
 	
-	public void addColegio(Colegio newColegio) {
+	public void addColegio(ColegioDto colegioDto) {
+		Colegio newColegio = new Colegio();
+		newColegio.setDane(colegioDto.getDane());
+		newColegio.setNombre(colegioDto.getNombre());
+		
+		Municipio muni = municipioService.getMunicipioById(colegioDto.getMunicipioId());
+		
+		newColegio.setMunicipio(muni);
+		
 		colegioRepository.save(newColegio);
 	}
 	
-	public void updateColegio(Integer id, Colegio colegio) {
+	public void updateColegio(Integer id, ColegioDto colegioDto) {
 		if(!colegioRepository.existsById(id))
 			throw new EntityNotFoundException("Colegio no encontrado con id: " + id);
+		
+		Colegio colegio = new Colegio();
+		colegio.setDane(colegioDto.getDane());
+		colegio.setNombre(colegioDto.getNombre());
+		
+		Municipio muni = municipioService.getMunicipioById(colegioDto.getMunicipioId());
+		
+		colegio.setMunicipio(muni);
+		
 		colegio.setId(id);
 		colegioRepository.save(colegio);
 	}
