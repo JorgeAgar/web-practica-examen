6f2d4c0bf99e5c7bdbd45c8a88bfa538ccb8cc6a
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-11 14:23:23 -0500

    Actualizado ProgramacionService para usar dto

--- practica-examen-web/src/main/java/com/example/demo/services/ProgramacionService.java
+++ practica-examen-web/src/main/java/com/example/demo/services/ProgramacionService.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dtos.ProgramacionDto;
+import com.example.demo.entities.Colegio;
+import com.example.demo.entities.Instructor;
 import com.example.demo.entities.Programacion;
+import com.example.demo.entities.Taller;
+import com.example.demo.entities.Ubicacion;
 import com.example.demo.repositories.ProgramacionRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -16,6 +21,18 @@ public class ProgramacionService {
 	@Autowired
 	private ProgramacionRepository programacionRepository;
 	
+	@Autowired
+	private ColegioService colegioService;
+	
+	@Autowired
+	private InstructorService instructorService;
+	
+	@Autowired
+	private TallerService tallerService;
+	
+	@Autowired
+	private UbicacionService ubicacionService;
+	
 	public List<Programacion> getAllProgramaciones() {
 		return programacionRepository.findAll();
 	}
@@ -24,14 +41,44 @@ public class ProgramacionService {
 		return programacionRepository.getReferenceById(id);
 	}
 	
-	public void addProgramacion(Programacion newProgramacion) {
+	public void addProgramacion(ProgramacionDto programacionDto) {
+		Programacion newProgramacion = new Programacion();
+		newProgramacion.setCantidad(programacionDto.getCantidad());
+		newProgramacion.setFechaFin(programacionDto.getFechaFin());
+		newProgramacion.setFechaInicio(programacionDto.getFechaInicio());
+		newProgramacion.setGrado(programacionDto.getGrado());
+		newProgramacion.setGrupo(programacionDto.getGrupo());
+		newProgramacion.setObservación(programacionDto.getObservación());
+		Colegio colegio = colegioService.getColegioById(programacionDto.getColegioId());
+		newProgramacion.setColegio(colegio);
+		Instructor instructor = instructorService.getInstructorById(programacionDto.getInstructorId());
+		newProgramacion.setInstructor(instructor);
+		Taller taller = tallerService.getTallerById(programacionDto.getTallerId());
+		Ubicacion ubicacion = ubicacionService.getUbicacionById(programacionDto.getUbicacionId());
+		newProgramacion.setUbicacion(ubicacion);
+		newProgramacion.setTaller(taller);
+		
 		programacionRepository.save(newProgramacion);
 	}
 	
-	public void updateProgramacion(Integer id, Programacion programacion) {
+	public void updateProgramacion(Integer id, ProgramacionDto programacionDto) {
 		if(!programacionRepository.existsById(id))
 			throw new EntityNotFoundException("Programación no encontrada con id: " + id);
-		programacion.setId(id);
+		Programacion programacion = programacionRepository.getReferenceById(id);
+		programacion.setCantidad(programacionDto.getCantidad());
+		programacion.setFechaFin(programacionDto.getFechaFin());
+		programacion.setFechaInicio(programacionDto.getFechaInicio());
+		programacion.setGrado(programacionDto.getGrado());
+		programacion.setGrupo(programacionDto.getGrupo());
+		programacion.setObservación(programacionDto.getObservación());
+		Colegio colegio = colegioService.getColegioById(programacionDto.getColegioId());
+		programacion.setColegio(colegio);
+		Instructor instructor = instructorService.getInstructorById(programacionDto.getInstructorId());
+		programacion.setInstructor(instructor);
+		Taller taller = tallerService.getTallerById(programacionDto.getTallerId());
+		Ubicacion ubicacion = ubicacionService.getUbicacionById(programacionDto.getUbicacionId());
+		programacion.setUbicacion(ubicacion);
+		programacion.setTaller(taller);
 		programacionRepository.save(programacion);
 	}
 	
