1c93a4cd7705560c1be0a2b17e9bb230c3891b4f
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-11 15:04:19 -0500

    Arreglado EvidenciaService para usar Dto

--- practica-examen-web/src/main/java/com/example/demo/services/EvidenciaService.java
+++ practica-examen-web/src/main/java/com/example/demo/services/EvidenciaService.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dtos.EvidenciaDto;
 import com.example.demo.entities.Evidencia;
+import com.example.demo.entities.Sesion;
 import com.example.demo.repositories.EvidenciaRepository;
-
-import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class EvidenciaService {
@@ -16,26 +16,27 @@ public class EvidenciaService {
 	@Autowired
 	private EvidenciaRepository evidenciaRepository;
 	
+	@Autowired
+	private SesionService sesionService;
+	
 	public List<Evidencia> getAllEvidencias() {
 		return evidenciaRepository.findAll();
+	}
+	
+	public List<Evidencia> getEvidenciaSesion(int sesionId) {
+		return sesionService.getSesionById(sesionId).getEvidencias();
 	}
 	
 	public Evidencia getEvidenciaById(Integer id) {
 		return evidenciaRepository.getReferenceById(id);
 	}
 	
-	public void addEvidencia(Evidencia newEvidencia) {
+	public void addEvidencia(EvidenciaDto evidenciaDto) {
+		Evidencia newEvidencia = new Evidencia();
+		newEvidencia.setFecha(evidenciaDto.getFecha());
+		newEvidencia.setObservación(evidenciaDto.getObservación());
+		Sesion sesion = sesionService.getSesionById(evidenciaDto.getSesionId());
+		newEvidencia.setSesion(sesion);
 		evidenciaRepository.save(newEvidencia);
-	}
-	
-	public void updateEvidencia(Integer id, Evidencia evidencia) {
-		if(!evidenciaRepository.existsById(id))
-			throw new EntityNotFoundException("Evidencia no encontrada con id: " + id);
-		evidencia.setId(id);
-		evidenciaRepository.save(evidencia);
-	}
-	
-	public void deleteEvidencia(Integer id) {
-		evidenciaRepository.deleteById(id);
 	}
 }
