eed8a467b64d9ba85b3c4743f847cb60d93cfb54
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-11 14:36:19 -0500

    Arreglado ParticipanteService para usar Dto

--- practica-examen-web/src/main/java/com/example/demo/services/ParticipanteService.java
+++ practica-examen-web/src/main/java/com/example/demo/services/ParticipanteService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dtos.ParticipanteDto;
+import com.example.demo.entities.Colegio;
 import com.example.demo.entities.Participante;
+import com.example.demo.entities.TipoDocumento;
 import com.example.demo.repositories.ParticipanteRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -16,6 +19,12 @@ public class ParticipanteService {
 	@Autowired
 	private ParticipanteRepository participanteRepository;
 	
+	@Autowired
+	private ColegioService colegioService;
+	
+	@Autowired
+	private TipoDocumentoService tipoDocumentoService;
+	
 	public List<Participante> getAllParticipantes() {
 		return participanteRepository.findAll();
 	}
@@ -24,14 +33,25 @@ public class ParticipanteService {
 		return participanteRepository.getReferenceById(id);
 	}
 	
-	public void addParticipante(Participante newParticipante) {
+	public void addParticipante(ParticipanteDto participanteDto) {
+		Participante newParticipante = new Participante();
+		newParticipante.setNombre(participanteDto.getNombre());
+		Colegio colegio = colegioService.getColegioById(participanteDto.getColegioId());
+		newParticipante.setColegio(colegio);
+		TipoDocumento tipoDocumento = tipoDocumentoService.getTipoDocumentoById(participanteDto.getTipoDocumentoId());
+		newParticipante.setTipoDocumento(tipoDocumento);
 		participanteRepository.save(newParticipante);
 	}
 	
-	public void updateParticipante(Integer id, Participante participante) {
+	public void updateParticipante(Integer id, ParticipanteDto participanteDto) {
 		if(!participanteRepository.existsById(id))
 			throw new EntityNotFoundException("Participante no encontrado con id: " + id);
-		participante.setId(id);
+		Participante participante = participanteRepository.getReferenceById(id);
+		participante.setNombre(participanteDto.getNombre());
+		Colegio colegio = colegioService.getColegioById(participanteDto.getColegioId());
+		participante.setColegio(colegio);
+		TipoDocumento tipoDocumento = tipoDocumentoService.getTipoDocumentoById(participanteDto.getTipoDocumentoId());
+		participante.setTipoDocumento(tipoDocumento);
 		participanteRepository.save(participante);
 	}
 	
